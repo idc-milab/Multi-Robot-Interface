40409cb9a2684592d954ffd575f91077078d1500
Etay Todress <70374970+EtayTod@users.noreply.github.com>
2021-04-22 13:51:01 +0300

    update main imports to support routes

--- src/App.tsx
+++ src/App.tsx
@@ -3,8 +3,11 @@ import './App.scss';
 import { HttpClient } from '@butter-robotics/mas-javascript-api';
 import { RobotObject } from './components/RobotObject';
 import { useState } from 'react';
-import { Navbar, Nav, Form, FormControl, Button, Container, Modal, ModalBody } from 'react-bootstrap';
+import { Navbar, Nav, Form, FormControl, Button, Container, Modal, ModalBody, NavDropdown } from 'react-bootstrap';
 import 'bootstrap/dist/css/bootstrap.min.css';
+import { BrowserRouter as Router, Link, Route, Switch } from 'react-router-dom';
+import { ScenarioButtons } from './components/ScenariosButtons';
+import { LinkContainer } from 'react-router-bootstrap';
 
 
 
