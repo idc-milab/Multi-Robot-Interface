1eccd5a0151a7a4a595a4955af3d767627315915
Etay Todress <70374970+EtayTod@users.noreply.github.com>
2021-04-22 13:28:26 +0300

    Scenario Buttons File

---
+++ src/components/ScenariosButtons.tsx
@@ -0,0 +1,60 @@
+import React from 'react';
+import { Navbar, Nav, Form, FormControl, Button, Container, ButtonGroup, Card } from 'react-bootstrap';
+import { HttpClient } from '@butter-robotics/mas-javascript-api';
+
+const kip = new HttpClient('192.168.57.30');
+kip.timeout = 240;
+
+let interval: NodeJS.Timeout;
+
+const scenarios: Map<string, any> = new Map<string, any>([
+    ['Baseline', {
+        start: () => interval = setInterval(function(){ kip.playAnimation('kip_start').then(kip.playAnimation('kip_H_Breath'));}, 10000),
+        stop: () => clearInterval(interval)
+    }],
+    ['In-Group', {
+        start: () => alert('starting In-Group'),
+        stop: () => alert('stopping In-Group')
+    }],
+    ['Out-Group', {
+        start: () => alert('starting Out-Group'),
+        stop: () => alert('stopping Out-Group')
+    }],
+
+    ]);
+
+export const ScenarioButtons = ({ scenario } : { scenario: string }) => {
+    const { start, stop } = scenarios.get(scenario);
+    return <div>
+    <Container className='scenario-component'>
+      <Card >
+        <Card.Header>
+            {scenario}
+        </Card.Header>
+        <Card.Body>
+            <Button onClick={start} variant="success">Start</Button>
+            <Button onClick={stop} variant="danger">Stop</Button>
+        </Card.Body>
+      </Card>
+    </Container>
+
+
+    </div>
+}
+
+// export function BaselineButton () {
+
+//     const kip = new HttpClient('192.168.57.30');
+
+//     const playBaselineAnimations = () => {
+//         kip.playAnimation('kip_start').then(kip.playAnimation('kip_H_Breath')).then(playBaselineAnimations()); //check with benny animation pipeline + recursion using while is better??
+//       }
+
+//     return <>
+    
+//     <Button variant='secondary' className='animation-button' onClick={() => playBaselineAnimations()}>
+//         Start Baseline Scenario
+//     </Button>
+//     </>
+// }
+
