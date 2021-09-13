286fb04f718c5679b8ce79c3a1f6fb61ecf0f07a
Omer-Sadeh <omer.sadeh@protonmail.com>
2021-09-13 16:18:09 +0300

    Revert "Update App.tsx"
    This reverts commit 1da4750b54e0abd3f6aa9f88468c4ce355e62d51.

--- src/App.tsx
+++ src/App.tsx
@@ -68,7 +68,7 @@ export class App extends React.PureComponent<{}, AppState> {
 		this.setState({IPdeleteState: StateArray});
 	}
 
-	renderButtons(ip: string) {
+	renderRemoveButtons(ip: string) {
 		let IParray = [...this.state.labCurrentIPs];
 		let StateArray = [...this.state.IPdeleteState];
 		let index = IParray.indexOf(ip);
@@ -164,7 +164,7 @@ export class App extends React.PureComponent<{}, AppState> {
 									{this.state.labCurrentIPs.map(ip => (
 										<ListGroup.Item>
 											<ButtonGroup aria-label="Basic example">
-											{this.renderButtons(ip)}
+											{this.renderRemoveButtons(ip)}
 						    				</ButtonGroup>
 										</ListGroup.Item>
 									))}
