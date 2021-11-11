227f2383f76ce991774139849f7465df632d5f57
Omer Sadeh <omer.sadeh@protonmail.com>
2021-11-11 16:50:53 +0200

    fix

---
+++ build/precache-manifest.1e164b49b91f360ff72205c1461a3f19.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d1bbfddb4a9159a480826852b580299d",
+    "url": "./index.html"
+  },
+  {
+    "revision": "0fc42d0744a1f560aeae",
+    "url": "./static/css/2.9233ae65.chunk.css"
+  },
+  {
+    "revision": "1e49a7c5ea3011638616",
+    "url": "./static/css/main.b6a00451.chunk.css"
+  },
+  {
+    "revision": "0fc42d0744a1f560aeae",
+    "url": "./static/js/2.430d322f.chunk.js"
+  },
+  {
+    "revision": "c87e50d81cc7b5311525cc6fd5482ea5",
+    "url": "./static/js/2.430d322f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1e49a7c5ea3011638616",
+    "url": "./static/js/main.8eeecbc8.chunk.js"
+  },
+  {
+    "revision": "7bcca7e6917c7d3996f8",
+    "url": "./static/js/runtime-main.39c339b6.js"
+  }
+]);
